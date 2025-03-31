e24cee69e2b65ba96f19e39000e684e8fc186b37
Tristan Sim <tsym0098@gmail.com>
2025-03-31 23:44:25 +0800

    Databases Lecture: SQL Basics

---
+++ Computer Science/Introduction to Databases/Module 4 Structured Query Langauge (Basic)/Applied 08/lecture_sql_basics.sql
@@ -0,0 +1,17 @@
+
+SELECT drone_id, drone_cost_hr/60 AS costpermin from drone.drone; 
+
+-- Will Return the Student Information (Displays Null)
+SELECT stuid, enrolmark, enrolgrade FROM uni.enrolment; 
+
+-- Will Return the Student Information (Uses Oracle NVL Functon to replace Null Values)
+SELECT stuid,
+   NVL(enrolmark, 0),
+   NVL(enrolgrade, 'WH')
+FROM uni.enrolment; 
+
+-- Oracle NVL Function Further Example
+SELECT rent_no, drone_id, rent_out_dt, nvl(rent_in_dt,'Still Out') from drone.rental; 
+
+-- Sorting Query Result (ORDER BY - Drone Fligt Time in Descending Order)
+SELECT drone_id, drone_flight_time FROM drone.drone ORDER BY drone_flight_time DESC, drone_id; 
