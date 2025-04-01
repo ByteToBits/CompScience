3d3312be45883b3e7204232982f130a9f6da5223
Tristan Sim <tsym0098@gmail.com>
2025-04-01 22:19:26 +0800

    Databases Module 4: SQL Basics Notebook Uploaded

--- Computer Science/Introduction to Databases/Module 4 Structured Query Langauge (Basic)/Applied 08/lecture_sql_basics.sql
+++ Computer Science/Introduction to Databases/Module 4 Structured Query Langauge (Basic)/Applied 08/lecture_sql_basics.sql
@@ -10,11 +10,12 @@ SELECT stuid,
    NVL(enrolgrade, 'WH')
 FROM uni.enrolment; 
 
--- Oracle NVL Function Further Example
-SELECT rent_no, drone_id, rent_out_dt, nvl(rent_in_dt,'Still Out') from drone.rental; 
-
 -- Sorting Query Result (ORDER BY - Drone Fligt Time in Descending Order)
 SELECT drone_id, drone_flight_time FROM drone.drone ORDER BY drone_flight_time DESC, drone_id; 
+
+-- Obtain tthe ids of those Drones which have been Rented
+-- DISTINCT: USE WITH CARE, Removes Duplicate Rows
+Select DISTINCT drone_id from drone.rental ORDER BY drone_id ASC; 
 
 -- SQL EQUI JOIN (Join Columns are Shown Twice)
 SELECT * FROM
@@ -29,3 +30,24 @@ SELECT * FROM
    NATURAL JOIN drone.drone_type
 ORDER BY
    dt_code; 
+
+-- Oracle NVL Function Further Example
+-- SELECT rent_no, drone_id, rent_out_dt, nvl(rent_in_dt,'Still Out') from drone.rental; This will Output an Error
+-- Because we are comparing a String to a Date Data Type; Convert Date to Characters
+SELECT rent_no, drone_id, to_char(rent_out_dt, 'dd-Mon-yyyy') as dateout, nvl((TO_CHAR(rent_in_dt, 'dd-mon-yyy')),'Still Out') from drone.rental; 
+
+-- Get Current Date, Sysdate, current_date, systimestamp
+SELECT TO_CHAR(sysdate, 'dd-Mon-YYYY hh:mi:ss AM') AS Curent_datetime FROM dual; 
+
+-- Example of Data/Number Output Formats and Comparisons
+SELECT 
+   drone_id, 
+   TO_CHAR(drone_pur_date, 'dd-Mon-yyyy') AS purchase_date,
+   TO_CHAR(drone_pur_price, '$9990.99') AS purchase_price,
+   TO_CHAR(drone_flight_time, '9990.99') AS drone_flight_time
+FROM 
+   drone.drone
+WHERE
+  drone_pur_date > TO_DATE('01-Mar-2021','dd-Mon-yyyy')
+ORDER BY
+  drone_id; 
