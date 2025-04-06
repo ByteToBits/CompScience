238493e59471666a8b9eeee077ae96dcd35df8d7
Tristan Sim <tsym0098@gmail.com>
2025-04-07 01:15:41 +0800

    Databases: Module 4 Applied 9-1 Updated to Question 10

--- Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Applied 09/Applied_9_1_SQL_Intermediate.sql
+++ Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Applied 09/Applied_9_1_SQL_Intermediate.sql
@@ -59,7 +59,6 @@ ORDER BY
 -- 4 Find the number of students enrolled in FIT1045 in the year 2019, under the following conditions (note two separate selects are required):
 -- a. Repeat students are counted multiple times in each semester of 2019
 DESC uni.enrolment; 
-
 SELECT COUNT(stuid) AS student_count
 FROM uni.enrolment 
 WHERE 
@@ -77,7 +76,6 @@ WHERE
 
 -- 5 Find the total number of prerequisite units for FIT5145.
 DESC uni.prereq; 
-
 SELECT COUNT(prerequnitcode) AS prereq_count
 FROM uni.prereq
 WHERE 
@@ -88,31 +86,93 @@ WHERE
 -- 6 Find the total number of enrolments per semester for each unit in the year 2019. The list should include the unit code, semester and the total 
 -- number of enrolments. Order the list in increasing order of enrolment numbers. For units with the same number of enrolments, display them by the 
 -- unit code order then by the semester order.
-
-
-
+SELECT 
+   unitcode, ofsemester, COUNT(stuid) AS Total_Enrolment
+FROM 
+   uni.enrolment
+WHERE
+   TO_CHAR(ofyear, 'yyyy') = '2019'
+GROUP BY 
+   unitcode, ofsemester
+ORDER BY 
+   unitcode, ofsemester;
 
 
 
 -- 7 Find the total number of prerequisite units for each unit which has prerequisites. In the list, include the unit code for which the count is 
 -- applicable. Order the list by unit code.
+SELECT
+   unitcode, COUNT(prerequnitcode) AS Number_of_Prerequisites
+FROM
+   uni.prereq
+GROUP BY 
+   unitcode
+ORDER BY
+  unitcode;
 
 
 
 -- 8 Find the total number of students whose marks are being withheld (grade is recorded as WH) for each unit offered in semester 2 2020. In the 
 -- listing include the unit code for which the count is applicable. Sort the list by descending order of the total number of students whose marks are 
 -- being withheld, then by the unit code.
+DESC uni.enrolment;
+SELECT 
+   unitcode, COUNT(DISTINCT stuid) AS Number_of_Students
+FROM 
+   uni.enrolment
+WHERE
+   TO_CHAR(ofyear,'yyyy') = '2020'
+   AND ofsemester = 2
+   AND UPPER(enrolgrade) = UPPER('WH')
+GROUP BY 
+   unitcode
+ORDER by
+   Number_of_Students DESC, unitcode DESC; 
 
 
 
 -- 9 For each prerequisite unit, calculate how many times it has been used as a prerequisite (number of times used). In the listing include the 
 -- prerequisite unit code, the prerequisite unit name and the number of times used. Sort the output by prerequisite unit code.
+DESC uni.prereq;
+DESC uni.unit;
+   
+SELECT
+   prerequnitcode AS unit_code,
+   u1.unitname AS unit_name,
+   COUNT(u1.unitcode) AS No_Of_Time_Used
+FROM
+   uni.prereq p1
+   JOIN uni.unit u1 ON u1.unitcode = p1.prerequnitcode 
+GROUP BY 
+   prerequnitcode, 
+   u1.unitname
+ORDER BY 
+   unit_code ASC;
 
 
 
 -- 10 Display the unit code and unit name of units which had at least 2 students who were granted a deferred exam (grade is recorded as DEF) in 
 -- semester 2 2021. Order the list by unit code.
+DESC uni.unit;
+DESC uni.enrolment;
 
+SELECT
+   unitcode, 
+   unitname,
+   COUNT(*)
+FROM
+   uni.unit NATURAL JOIN uni.enrolment
+WHERE 
+   ofsemester = 2
+   AND TO_CHAR(ofyear, 'yyyy') = '2021'
+   AND UPPER(enrolgrade) = UPPER('DEF')
+GROUP BY
+   unitcode,
+   unitname
+HAVING 
+   COUNT(*) >= 2
+ORDER BY
+   unitcode DESC;
 
 
 
