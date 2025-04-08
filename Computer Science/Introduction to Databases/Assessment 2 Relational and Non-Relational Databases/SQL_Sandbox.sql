e65c90815164137fd11997b02c5aae69aa388c6e
Tristan Sim <tsym0098@gmail.com>
2025-04-08 21:33:10 +0800

    Databases: Assessment 2 - General SQL Query Sandbox

--- Computer Science/Introduction to Databases/Assessment 2 Relational and Non-Relational Databases/SQL_Sandbox.sql
+++ Computer Science/Introduction to Databases/Assessment 2 Relational and Non-Relational Databases/SQL_Sandbox.sql
@@ -9,4 +9,23 @@ Student Name: Tristan Sim
 Last Modified Date: 2nd April 2025
 */
 
+-- Delete: To Delete Data Fast and and Logs in Database for Audit
+DELETE FROM SERVICE_JOB;
+DELETE FROM SERVICE;
+DELETE FROM PART_CHARGE;
+COMMIT;
+
+
+-- Truncate: To Delete Data Fast and Does Not Log (Ussually for Test Data)
+-- TRUNCATE TABLE table_name;
+
+-- Search for Data inserted in Task 2
 SELECT * FROM SERVICE;
+SELECT * FROM SERVICE_JOB;
+SELECT * FROM PART_CHARGE;
+
+-- Find Information of the Customer Table
+DESC CUSTOMER;
+SELECT CUST_NO, CUST_NAME FROM CUSTOMER;
+
+
