edcdfbc2f3010ef4f08e317be35f91da8042c182
Tristan Sim <tsym0098@gmail.com>
2025-04-09 00:34:01 +0800

    Databases: Lecture 5 SQL Advance

--- Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Lecture_SQL_Advance.sql
+++ Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Lecture_SQL_Advance.sql
@@ -19,7 +19,6 @@ Drones which can carry objects have been classified by HyFlying as:
 For all drones display the drone id, the carrying capacity classification eg 'No load' and the
 drone cost per hour
 */
-
 SELECT
    drone_id, 
    CASE
@@ -36,3 +35,148 @@ FROM
    NATURAL JOIN drone.drone
 ORDER BY
    drone_id;
+
+
+/* For Each Drone find the Custoemrs (cust_id only) who rented the drone for the longest duration */
+SELECT
+   drone_id,
+   TO_CHAR(rent_in_dt - rent_out_dt, '9990.9') AS Days_Rented_Out
+FROM 
+   drone.rental
+WHERE
+   rent_in_dt IS NOT NULL
+ORDER BY
+   drone_id;
+
+
+/* This Variation will Return Each Drone with its Max Rented Days out */ 
+SELECT
+   drone_id,
+   TO_CHAR(MAX(rent_in_dt - rent_out_dt), '9990.9') AS Max_Days_Out
+FROM
+   drone.rental
+WHERE
+   rent_in_dt IS NOT NULL
+GROUP BY
+   drone_id
+ORDER BY
+   drone_id;
+
+
+/* Subquery (Nested): Example for Refresher */
+SELECT
+   drone_id,
+   TO_CHAR((rent_in_dt - rent_out_dt),'9990.99') AS Max_Days_Out,
+   cust_id
+FROM
+   drone.rental
+   NATURAL JOIN drone.cust_train
+WHERE
+   rent_in_dt IS NOT NULL
+   AND ( drone_id, ( rent_in_dt - rent_out_dt ) ) IN (
+       SELECT
+          drone_id, MAX(rent_in_dt - rent_out_dt)
+       FROM
+          drone.rental
+       WHERE
+          rent_in_dt IS NOT NULL
+       GROUP BY
+          drone_id
+   )
+ORDER BY
+   drone_id,
+   cust_id;
+
+/* 
+Subquery (Correlated) (Use a Alias where the Inner Query References the Outer Query)
+Correlated Subquery is related to the Outer Query, and is evaluted once for each row of the outer query
+Correlated can also be used within Update Statements; Outer Updates occurs based on the Value returned from the subquery
+Alias R2 is evaluated first then it references the outer Table R1 to be evaluted (Inefficient in the example below)
+*/
+SELECT
+   drone_id,
+   TO_CHAR((rent_in_dt - rent_out_dt),'9990.99') AS Max_Days_Out,
+   cust_id
+FROM
+   drone.cust_train
+   NATURAL JOIN drone.rental r1
+WHERE
+   rent_in_dt IS NOT NULL
+   AND ( ( rent_in_dt - rent_out_dt ) ) IN (
+       SELECT
+          MAX(rent_in_dt - rent_out_dt)
+       FROM
+          drone.rental r2
+       WHERE
+          rent_in_dt IS NOT NULL
+          AND r1.drone_id = r2.drone_id
+   )
+ORDER BY
+    drone_id,
+    cust_id;
+          
+   
+
+/*
+Subquery (Inline)
+Creates a Temporary Table called 'maxtable' from a Subquery and Join Temporary Table with Rental Table
+*/
+SELECT
+   r.drone_id,
+   TO_CHAR((r.rent_in_dt - r.rent_out_dt), '9990.99') AS Max_Days_Out,
+   r.ct_id
+FROM
+   (
+       SELECT
+         drone_id, 
+         MAX(rent_in_dt - rent_out_dt) AS maxout
+       FROM
+         drone.rental
+       WHERE
+         rent_in_dt IS NOT NULL
+       GROUP BY
+         drone_id
+   ) maxtable
+JOIN drone.rental r
+   ON maxtable.drone_id = r.drone_id
+  AND (r.rent_in_dt - r.rent_out_dt) = maxtable.maxout
+JOIN drone.cust_train c
+   ON r.ct_id = c.ct_id
+ORDER BY
+   r.drone_id,
+   r.ct_id;
+
+
+/* Subquery (INLINE) Exercise: For each Drone Compute the Percentage of the Company's Rentals Contirbuted by that Drone */
+
+/* Bottom Query: Will Return Each Drone based on Drone ID how many times its been rented */
+SELECT
+   drone_id,
+   COUNT(*) AS times_rented
+FROM
+   drone.rental
+WHERE
+   rent_in_dt IS NOT NULL
+GROUP BY
+   drone_id
+ORDER BY
+   drone_id;
+   
+/* To find the Percentages, ( Times_Rented / Total Number of Times All Drones Rented) * 100 - Use Inline Subquery */
+SELECT
+   drone_id,
+   COUNT(*) AS times_rented,
+   TO_CHAR(COUNT(*) * 100 / (
+       SELECT
+          COUNT(rent_in_dt)
+       FROM
+          drone.rental
+   ), '990.99') AS percent_overall
+FROM
+   drone.rental
+WHERE
+   rent_in_dt IS NOT NULL
+GROUP BY
+   drone_id
+ORDER BY
+   percent_overall DESC;
