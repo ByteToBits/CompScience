34dee670aecc76a097009be355e4b652a668f15d
Tristan Sim <tsym0098@gmail.com>
2025-04-18 13:10:35 +0800

    Databases: SQL Adavance Applied Practice 10-1 Question 2

--- Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Applied_10_1_SQL_Advance.sql
+++ Computer Science/Introduction to Databases/Module 5 SQL Intermediate and Advance/Applied_10_1_SQL_Advance.sql
@@ -9,8 +9,9 @@ Student Name: Tristan Sim
 Last Modified Date: 10th April 2025
 */
 
-SPOOL output.txt
-SET LINESIZE 300
+-- SPOOL output.txt
+-- SET LINESIZE 300
+
 
 /* 1. Assuming that the student name is unique, display Claudette Serman’s academic record. 
 Include the unit code, unit name, year, semester, mark and explained_grade in the listing. The Explained Grade 
@@ -51,4 +52,68 @@ ORDER BY
    year,
    ofsemester,
    unitcode; 
-   
+/* Alternate Approach using Oracle's Decode */
+SELECT
+    unitcode,
+    unitname,
+    EXTRACT(YEAR FROM ofyear)  AS year,
+    ofsemester,
+    enrolmark,
+    DECODE(UPPER(enrolgrade), 'N', 'Fail', 'P', 'Pass',
+           'C', 'Credit', 'D', 'Distinction', 'HD',
+           'High Distinction') AS explained_grade
+FROM
+    uni.enrolment
+    NATURAL JOIN uni.unit
+    NATURAL JOIN uni.student
+WHERE
+    upper(stufname) = upper('Claudette')
+    AND UPPER(stulname) = upper('Serman')
+ORDER BY
+    year,
+    ofsemester,
+    unitcode;
+
+
+
+/* 2. Find the number of scheduled classes assigned to each staff member for each semester in 2019. 
+If the number of classes is 2 then this should be labelled as a correct load, more than 2 as an overload and less than 2 as an underload.
+Include the staff id, staff first name, staff last name, semester, number of scheduled classes and load in the listing. Sort the 
+list by decreasing order of the number of scheduled classes and when the number of classes is the same, sort by the staff id then by the semester.
+*/
+
+DESC uni.unit;
+DESC uni.schedclass;
+DESC uni.staff;
+
+SELECT 
+   s.staffid,
+   s.stafffname,
+   s.stafflname,
+   sh.ofsemester,
+   COUNT(*) AS numberclasses, 
+   CASE
+      WHEN COUNT(*) > 2 THEN
+        'Overload'
+      WHEN COUNT(*) = 2 THEN
+        'Correct Load'
+      ELSE
+        'Underload'
+   END AS load
+FROM
+   uni.schedclass sh
+   JOIN uni.staff s ON s.staffid = sh.staffid
+WHERE
+   TO_CHAR(ofyear, 'yyyy') = '2019'
+GROUP BY
+   s.staffid,
+   s.stafffname,
+   s.stafflname,
+   sh.ofsemester
+ORDER BY
+   numberclasses DESC, s.staffid ASC, sh.ofsemester ASC;
+
+
+/* 3. Find the total number of prerequisite units for all units. Include in the list the unit code of units that do not have a prerequisite. 
+Order the list in descending order of the number of prerequisite units and where several units have the same number of prerequisites order then by unit code.
+*/
