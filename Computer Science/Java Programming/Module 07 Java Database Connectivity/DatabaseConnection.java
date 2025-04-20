f9298499306e153e45b07b2d3bb4734e695f0f16
Tristan Sim <tsym0098@gmail.com>
2025-04-20 15:58:19 +0800

    Java Programming: Database Connection File Created

---
+++ Computer Science/Java Programming/Module 07 Java Database Connectivity/DatabaseConnection.java
@@ -0,0 +1,10 @@
+/**
+ * Module 07: Database Connection (Oracle SQL)
+ * Institution: Monash Unviersity Australia
+ * Subject: Java Programming
+ * Description: Class that Connects to an Oracle SQL Database using the Java Database Connectivity Driver(JBDC)
+ * @author Tristan
+ * @version 1.01
+*/
+
+
