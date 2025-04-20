028556eeede673edd322dc3406f99518e3c9e5fa
Tristan Sim <tsym0098@gmail.com>
2025-04-20 16:10:16 +0800

    Created a Database Credential Manager to Mange the Login Credentials

---
+++ Computer Science/Java Programming/Module 07 Java Database Connectivity/DatabaseCredentialManager.java
@@ -0,0 +1,20 @@
+/**
+ * Module 07: Database Credential Manager
+ * Institution: Monash Unviersity Australia
+ * Subject: Java Programming
+ * Description: Class that Reads a JSON File for Database Configuration Details 
+ * @author Tristan
+ * @version 1.01
+*/
+
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class DatabaseCredentialManager {
+    
+   
+
+}
